d1b0f3097e0dfff7ec144334fae75ea98aea421d
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-09-24 18:15:54 -0400

    buddie generator created

--- routes/favorites-api-routes.js
+++ routes/favorites-api-routes.js
@@ -1,21 +1,32 @@
 const db = require('../models');
-
+const { Op } = require('sequelize');
 
 
 module.exports = function (app) {
     // This function will hopefully generate a list of buddies when selecting one trail.
     app.get("/api/buddyList/:id", async (req, res) => {
         try{
-            const buddyUserIds = await db.Favorite.findAll({
+            // This finds all of the matching favorites with the trail id.
+            const matchingFavorites = await db.Favorite.findAll({
                 where: {
-                    hiking_trail_id: req.params.id
+                    HikingTrailId: req.params.id
                 }
-            }).then(({ user_id }) => {
-                return user_id;
             });
-    
-            console.log(buddyUserIds);
-            res.json(buddyUserIds);
+            const buddyList = [];
+            // This breaks the information down into UserId array
+            matchingFavorites.forEach(({UserId}) => {
+                buddyList.push(UserId);
+                });
+            // This uses the array to return all of the User information.
+            const matchingBuddies = await db.User.findAll({
+                where: {
+                    id: {
+                        [Op.or]: buddyList
+                    }
+                }
+            });
+            // This sends all of the "buddies" to the front-end. Woohoo!
+            res.json(matchingBuddies);
         } catch(err) {
             res.send(err);
         }

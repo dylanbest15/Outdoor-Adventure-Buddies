b78dded2dff6ae4bd7806298f5cdbd31e0a4c371
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-09-23 23:17:00 -0400

    start of some user api routes

---
+++ routes/user-api-routes.js
@@ -0,0 +1,28 @@
+const db = require("../models");
+
+module.exports = function(app) {
+  // This retrieves the Information of the current user.
+  app.get("/api/user/:id", (req, res) => {
+    db.User.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then((UserId) => {
+        res.json(UserId);
+      })
+      .catch((err) => console.log(err));
+  });
+//   This generates the bucket list of favorites for the unique user
+  app.get("api/favorites/:id", (req, res) => {
+    db.Favorite.findAll({
+      where: {
+        userID: req.params.id
+      }
+    })
+    .then((userFavorites) => {
+        res.json(userFavorites);
+    })
+    .catch((err) => console.log(err));
+  });
+};

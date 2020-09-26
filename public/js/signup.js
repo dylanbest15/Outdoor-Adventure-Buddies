006a2112129cfe3fa9e962efbe860ff53f8563f0
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-09-26 14:04:11 -0400

    Configuring signup sheet

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,15 +1,27 @@
 $(document).ready(() => {
   // Getting references to our form and input
   const signUpForm = $("form.signup");
-  const emailInput = $("input#email-input");
-  const passwordInput = $("input#password-input");
+  const firstNameInput = $("input#firstname");
+  const lastNameInput = $("input#lastname");
+  const userNameInput = $("input#username");
+  const emailInput = $("input#email");
+  const passwordInput = $("input#password");
+  const ageInput = $("input#age");
+  const bioInput = $("input#bio");
+  const genderInput = $("input#gender");
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", event => {
     event.preventDefault();
     const userData = {
+      firstName: firstNameInput.val().trim(),
+      lastName: lastNameInput.val().trim(),
+      userName: userNameInput.val().trim(),
       email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      password: passwordInput.val().trim(),
+      age: ageInput.val().trim(),
+      bio: bioInput.val().trim(),
+      gender: genderInput
     };
 
     if (!userData.email || !userData.password) {
@@ -23,8 +35,11 @@ $(document).ready(() => {
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  function signUpUser(firstName, lastName, userName, email, password) {
     $.post("/api/signup", {
+      first_name: firstName,
+      last_name: lastName,
+      user_name: userName,
       email: email,
       password: password
     })

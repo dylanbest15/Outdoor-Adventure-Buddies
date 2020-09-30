a8ff214ad771c327fb72d13eda359e89c50b69d7
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-09-29 23:27:02 -0400

    path created for dacks w/ trail object return

--- public/js/peakSelection.js
+++ public/js/peakSelection.js
@@ -1,9 +1,14 @@
 $(document).ready(() => {
-    $(document).on('click', "button", (event) => {
-        console.log(event.target);
+    $(document).on('click', "button", async (event) => {
+        const peakId = $(event.target).attr("data-peak");
+        console.log(peakId);
+
+        const trailList = await $.get(`/api/hikingTrails/${peakId}`);
+        console.log(trailList);
+        window.location.href = "/trails";
+
+        // Now we add the function that generates the trail list
     })
-
-
 
 
 });

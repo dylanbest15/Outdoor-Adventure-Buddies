1f17da24b2aca23345b32fb2594775d900159c90
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-09-30 00:00:27 -0400

    loads object, needs to redirec to page w/o losing

--- public/js/peakSelection.js
+++ public/js/peakSelection.js
@@ -1,14 +1,13 @@
 $(document).ready(() => {
+    let trailList;
     $(document).on('click', "button", async (event) => {
         const peakId = $(event.target).attr("data-peak");
         console.log(peakId);
-
-        const trailList = await $.get(`/api/hikingTrails/${peakId}`);
+        trailList = await $.get(`/api/hikingTrails/${peakId}`);
+       
         console.log(trailList);
-        window.location.href = "/trails";
-
         // Now we add the function that generates the trail list
     })
-
+   
 
 });

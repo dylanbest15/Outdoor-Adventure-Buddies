3e8b5699a312145f3c08676ccad2e3aecec6ef55
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-09-30 14:02:00 -0400

    fixed bug in the favorites api routes

--- routes/favorites-api-routes.js
+++ routes/favorites-api-routes.js
@@ -6,8 +6,9 @@ module.exports = function (app) {
     // This function will hopefully generate a list of buddies when selecting one trail.
     app.get("/api/buddyList/:id", async (req, res) => {
         try{
-            
-            const currentUser = req.body.userID.toString();
+            console.log(req.user.id);
+            const currentUser = req.user.id.toString();
+            console.log(currentUser);
             // This finds all of the matching favorites with the trail id.
             const matchingFavorites = await db.Favorite.findAll({
                 where: {

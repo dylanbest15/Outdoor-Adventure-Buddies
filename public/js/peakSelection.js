378768a9767cef86892eecdae750e56c85588839
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-09-30 19:17:50 -0400

    emptied trails list before generating.

--- public/js/peakSelection.js
+++ public/js/peakSelection.js
@@ -7,6 +7,7 @@ $(document).ready(() => {
             console.log(peakId);
             const trailList = await $.get(`/api/hikingTrails/${peakId}`);
             console.log(trailList);
+            trailSection.empty();
             trailList.forEach(({ id, trail_name, mileage, elevation_gain, description, trail_photo}) => {
                 const newTrail = trailItem.clone();
                 newTrail.find("img.trail-photo").attr("src", trail_photo);

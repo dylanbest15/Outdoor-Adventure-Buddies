964e5bf6f286697208a220734bd65f5f6882cf3c
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-09-30 20:33:42 -0400

    favorites modified

--- routes/favorites-api-routes.js
+++ routes/favorites-api-routes.js
@@ -33,6 +33,8 @@ module.exports = function (app) {
                     }
                 })
                 res.json(matchingBuddies)
+            } else {
+                res.sendStatus(418);
             }
             
             

454af9d7847b7106a79b485997751fffeaaaeefb
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-09-30 22:14:39 -0400

    converted members.html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -24,7 +24,7 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.render('members', {layout: "main"});
   });
 
   app.get("/bucketlist", isAuthenticated, (req, res) => {

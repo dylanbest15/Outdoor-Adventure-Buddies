86e0960c70dc199697e9d46c32ef780ccf3762cf
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-10-02 17:58:30 -0400

    created new branch and merged with master

--- public/js/email.js
+++ public/js/email.js
@@ -3,65 +3,64 @@ $(document).ready(() => {
     const emailModal = document.getElementById("contact-modal");
     const contactButton = $("a.contact-button");
 
-    // close modal click event
-    $("button.close").on("click", function (event) {
-        emailModal.style.display = "none";
-    })
+    // // close modal click event
+    // $("button.close").on("click", function (event) {
+    //     emailModal.style.display = "none";
+    // })
 
-    // window click event for modal
-    window.onclick = function (event) {
-        if (event.target == emailModal) {
-            emailModal.style.display = "none";
-        }
-    }
+    // // window click event for modal
+    // window.onclick = function (event) {
+    //     if ($(".contact-modal").css('display') == 'block' && event.target === emailModal) {
+    //         emailModal.style.display = "none";
+    //     }
+    // }
 
     // find adventure buddies click event
-    $(document).on("click", "contact-button", function (event) {
-        event.preventDefault();
+    $("a.contact-button").on("click", function (event) {
 
         console.log('contact button clicked');
         // display modal
         emailModal.style.display = "block";
     })
 
-    // email button click event
-    $(document).on("click", "button.email-email", function (event) {
+    // // email button click event
+    // $(document).on("click", "button.email-email", function (event) {
 
-        event.preventDefault();
+    //     event.preventDefault();
 
-        // recipients email and name for email template
-        emailAddress = $(this).attr("id");
-        recipientName = $(this).attr("data-recipient");
+    //     // recipients email and name for email template
+    //     emailAddress = $(this).attr("id");
+    //     recipientName = $(this).attr("data-recipient");
 
-        // api request to grab current users email address
-        async function getSenderUserEmail() {
-            await $.get("/api/user_data", function ({ email }) {
-                console.log(email);
-                return emailData = {
-                    userTo: emailAddress,
-                    userFrom: email,
-                    userFromName: recipientName
-                };
-            }).then(() => {
-                if (!emailData.userTo || !emailData.userFrom) {
-                    console.log(`***\nno userTo or no userFrom\n***`);
-                    return;
-                }
-                console.log('emailData === ', emailData)
-                // If we have an email message then run sendEmail
-                sendEmail(emailData);
-            });
-        };
+    //     // api request to grab current users email address
+    //     async function getSenderUserEmail() {
+    //         await $.get("/api/user_data", function ({ email }) {
+    //             console.log(email);
+    //             return emailData = {
+    //                 userTo: emailAddress,
+    //                 userFrom: email,
+    //                 userFromName: recipientName
+    //             };
+    //         }).then(() => {
+    //             if (!emailData.userTo || !emailData.userFrom) {
+    //                 console.log(`***\nno userTo or no userFrom\n***`);
+    //                 return;
+    //             }
+    //             console.log('emailData === ', emailData)
+    //             // If we have an email message then run sendEmail
+    //             sendEmail(emailData);
+    //         });
+    //     };
 
-        // Sends data required for an email to be sent. 
-        function sendEmail(emailData) {
-            $.post("/email/send-email", emailData)
-                .catch((err) => {
-                    console.log(err);
-                });
-        }
+    //     // Sends data required for an email to be sent. 
+    //     function sendEmail(emailData) {
+    //         $.post("/email/send-email", emailData)
+    //             .catch((err) => {
+    //                 console.log(err);
+    //             });
+    //     }
 
-        getSenderUserEmail();
+    //     getSenderUserEmail();
 
-    })
+    // })
 });

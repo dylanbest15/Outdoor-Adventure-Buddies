fff303edf58520ed3674f8549b29180b55974a06
Dylan Best <best.dylan15@gmail.com>
2020-10-02 14:36:10 -0400

    coming soon modal to travel and climbing links

--- public/js/members.js
+++ public/js/members.js
@@ -4,4 +4,31 @@ $(document).ready(() => {
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.email);
   });
+
+  // coming soon modal
+  const comingSoon = document.getElementById("coming-soon");
+
+  // travel button opens modal
+  $("a.travel-button").on("click", function (event) {
+    event.preventDefault();
+    comingSoon.style.display = "block";
+  })
+
+  // climbing button opens modal
+  $("a.climbing-button").on("click", function (event) {
+    event.preventDefault();
+    comingSoon.style.display = "block";
+  })
+
+  // close modal click event
+  $("button.close").on("click", function (event) {
+    comingSoon.style.display = "none";
+  })
+
+  // window click event for modal
+  window.onclick = function (event) {
+    if (event.target == comingSoon) {
+      comingSoon.style.display = "none";
+    }
+  }
 });
